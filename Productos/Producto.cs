8921ff635fcaf9e219c727dcddde3b545e793353
Leo538 <lozadaleonel15@gmail.com>
2025-09-02 16:05:44 -0500

    Agrego .gitignore para ignorar archivos temporales y de compilación

---
+++ Productos/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductosApi.Productos
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public bool Disponible { get; set; }
+    }
+}
